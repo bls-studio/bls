96838db69fe437a88f97b32873adf925c35f6d2c
blake-swl <seulwoolee93@gmail.com>
2019-08-26 17:27:14 -0700

    [edit] landing

--- public/src/components/Landing/index.jsx
+++ public/src/components/Landing/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './landing.scss';
 
 // Assets
-import smilingMobile from '../../../dist/assets/girl3.png';
+import smilingMobile from '../../../dist/assets/ascend--foreground--cropped.png';
 
 
 const Landing = () => {
@@ -13,34 +13,25 @@ const Landing = () => {
     window.addEventListener('scroll', parallaxShift);
 
     return () => {
-      window.removeEventListener('click', parallaxShift)
+      window.removeEventListener('scroll', parallaxShift)
     }
   }, []);
 
   return(
     <div className="landing">
-      <svg id="ellipse1" xmlns="http://www.w3.org/2000/svg" width="385" height="385" viewBox="0 0 385 385">
-        <g id="Ellipse_1" data-name="Ellipse 1" fill="none" stroke="#c7a47a" stroke-width="8">
-          <circle cx="192.5" cy="192.5" r="192.5" stroke="none"/>
-          <circle cx="192.5" cy="192.5" r="188.5" fill="none"/>
-        </g>
-      </svg>
-      <div className="landing__headline">
-        <h1 className="landing__headline--major">Putting on smiles through design, code, & innovation.</h1>
-        <h3 className="landing__headline--minor">An creative web development studio.</h3>
-        <button className="landing__cta">Let's talk</button>
-      </div>
-      <svg id="ellipse2" xmlns="http://www.w3.org/2000/svg" width="385" height="385" viewBox="0 0 385 385">
-        <g id="Ellipse_2" data-name="Ellipse 2" fill="none" stroke="#5f503e" stroke-width="8">
-          <circle cx="192.5" cy="192.5" r="192.5" stroke="none"/>
-          <circle cx="192.5" cy="192.5" r="188.5" fill="none"/>
-        </g>
-      </svg>
-      <div className="landingImage__container">
-        <img src={smilingMobile} alt="" className="smilingMobile"/>
-      </div>
-      <div className="mouse">
-        <div className="mouse__text">scroll</div>
+      <div className="landing__container">
+        <div className="landing__headline">
+          <h1 className="landing__headline--major">A creative web development studio.</h1>
+          <h3 className="landing__headline--minor">Connecting through code, design, & innovation.</h3>
+          <button className="landing__cta">Let's talk</button>
+        </div>
+        <div className="landing__image">
+          <img src={smilingMobile} alt="" className="parallax__image" style={{ bottom: offset / 2 }}/>
+        </div>
+        <div className="mouse">
+          <div className="mouse__text">scroll</div>
+        </div>
+
       </div>
     </div>
   )
